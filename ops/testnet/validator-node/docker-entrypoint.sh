a8e27d9465f274e23c896e430b5d2f4179e0e251
dim4egster <dim4egster@gmail.com>
2023-11-30 20:19:09 +0800

    docker: added mining start option for validator node

--- ops/testnet/validator-node/docker-entrypoint.sh
+++ ops/testnet/validator-node/docker-entrypoint.sh
@@ -6,6 +6,7 @@ RTF_CONFIG=${RTF_HOME}/config/config.toml
 KEYSTORE_DIR=${DATA_DIR}/keystore
 GETH_DIR=${DATA_DIR}/geth
 
+# check if validator account exists, if not then import it.
 if [ ! -d "$KEYSTORE_DIR" ]; then
   geth --rtftestnet --password <(echo) --datadir "${DATA_DIR}" account import <(echo ${VALIDATOR_KEY})
 fi
@@ -13,9 +14,9 @@ fi
 # clear shell history
 history -c && history -w
 
-# To start syncing from scratch make first run with non full history mode,
-# next run switch to full history archive mode
-# To switch on full history mode need restart container once
+# To start syncing from scratch should make first run with non full history mode,
+# next run will auto switch node to full history archive mode.
+# To switch on full history mode need restart container once.
 if [ ! -d "$GETH_DIR" ]; then
   exec "geth" "--rtftestnet"\
    "--config" "${RTF_CONFIG}"\
@@ -31,6 +32,7 @@ if [ ! -d "$GETH_DIR" ]; then
    exec "geth" "--rtftestnet"\
     "--config" "${RTF_CONFIG}"\
     "--miner.etherbase" "${VALIDATOR_ADDRESS}"\
+    "--mine"\
     "--datadir" "${DATA_DIR}"\
     "--unlock" "${VALIDATOR_ADDRESS}"\
     "--password" <(echo)\
